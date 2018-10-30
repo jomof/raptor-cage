5301104786a210e00fc635f37219705acb09ba39
jomof <jomofisher@gmail.com>
2018-10-29 17:11:24 -0700

    Add template

--- src/test/kotlin/com/github/jomof/buildserver/BenchmarkTests.kt
+++ src/test/kotlin/com/github/jomof/buildserver/BenchmarkTests.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class BenchmarkTests {
     @Test
     fun basic() {
-        Benchmark(moduleCount = 2)
+        Benchmark(moduleCount = 1)
             .prepare()
             .execute("./gradlew${os.bat}", "assemble", "clean")
             .execute("./gradlew${os.bat}", "assemble")
@@ -14,7 +14,8 @@ class BenchmarkTests {
 
     @Test
     fun ndks() {
-        val ndks = listOf("r13b", "r14b", "r15c", "r16b", "r17c")
+        val ndks = listOf("r17b")
+        //val ndks = listOf("r13b", "r14b", "r15c", "r16b", "r17c")
         ndks.onEach { ndk ->
             val path = getNdkDownloadIfNecessary(ndk)
             System.err.println("Downloaded and unzipped $path")
