6183c76a66a51a502eb740d08d8e2f1d1b28a2de
Jomo Fisher <jomofisher@gmail.com>
2018-12-19 15:17:40 -0800

    File watcher

--- src/test/kotlin/com/github/jomof/buildserver/server/watcher/WatchFolderTest.kt
+++ src/test/kotlin/com/github/jomof/buildserver/server/watcher/WatchFolderTest.kt
@@ -3,9 +3,28 @@ package com.github.jomof.buildserver.server.watcher
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import java.io.File
+import java.lang.RuntimeException
 import java.nio.file.FileSystems
 
 class WatchFolderTest {
+    fun pollUntil(watchFolder : WatchFolder, expected: Int) : Int {
+        var total = 0
+        val start = System.currentTimeMillis()
+        while (total < expected && (System.currentTimeMillis() - start) < 5000) {
+            total += watchFolder.pollEvents().size
+            if (total < expected) {
+                Thread.sleep(50)
+            }
+        }
+        if (total < expected) {
+            throw RuntimeException("timeout after ${System.currentTimeMillis() - start}")
+        }
+        if (total > expected) {
+            throw RuntimeException("Expected $expected, saw $total")
+        }
+        return total
+
+    }
     @Test
     fun test() {
         val folder = File("./my-watch-folder-test")
@@ -15,12 +34,12 @@ class WatchFolderTest {
         val watchFolder = WatchFolder(watcher, folder.toPath())
         File(folder, "./my-File.txt").writeText("Hello")
         File(folder, "sub").mkdirs()
-        assertThat(watchFolder.pollEvents().size).isEqualTo(3)
-        assertThat(watchFolder.pollEvents().size).isEqualTo(0)
+        pollUntil(watchFolder, 3)
+        pollUntil(watchFolder, 0)
         File(folder, "./my-dir").mkdirs()
-        assertThat(watchFolder.pollEvents().size).isEqualTo(1)
+        pollUntil(watchFolder, 1)
         File(folder, "./my-dir/a/b/c/d").mkdirs()
-        assertThat(watchFolder.pollEvents().size).isEqualTo(4)
+        pollUntil(watchFolder, 4)
         folder.deleteRecursively()
     }
 }
