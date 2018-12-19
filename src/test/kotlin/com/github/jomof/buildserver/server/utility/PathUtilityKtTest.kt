bbb8688ec6db733e736850480358116bdb5c8556
Jomo Fisher <jomofisher@gmail.com>
2018-12-19 14:59:42 -0800

    File watcher

--- src/test/kotlin/com/github/jomof/buildserver/server/utility/PathUtilityKtTest.kt
+++ src/test/kotlin/com/github/jomof/buildserver/server/utility/PathUtilityKtTest.kt
@@ -2,15 +2,21 @@ package com.github.jomof.buildserver.server.utility
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import java.io.File
+import java.nio.file.Path
 import java.nio.file.Paths
 
 class PathUtilityKtTest {
 
+    private fun toPaths(s : String) : Set<Path> {
+        return s.trimIndent().split("\n").map { it.trim() }.map { File(it).toPath() }.toSet()
+    }
+
     @Test
     fun oneLine() {
-        val paths = """
-            mylibrary\.externalNativeBuild\cmake\debug\arm64-v8a\build.ninja
-        """.trimIndent().split("\n").map { Paths.get(it) }.toSet()
+        val paths = toPaths("""
+            mylibrary/.externalNativeBuild/cmake/debug/arm64-v8a/build.ninja
+        """)
 
         val map = paths.removeCommonSegments()
         assertThat(map).hasSize(paths.size)
@@ -22,10 +28,10 @@ class PathUtilityKtTest {
 
     @Test
     fun twoLines() {
-        val paths = """
-            mylibrary\.externalNativeBuild\cmake\debug\arm64-v8a\build.ninja
-            mylibrary\.externalNativeBuild\cmake\debug\armeabi-v7a\build.ninja
-        """.trimIndent().split("\n").map { Paths.get(it) }.toSet()
+        val paths = toPaths("""
+            mylibrary/.externalNativeBuild/cmake/debug/arm64-v8a/build.ninja
+            mylibrary/.externalNativeBuild/cmake/debug/armeabi-v7a/build.ninja
+        """)
 
         val map = paths.removeCommonSegments()
         assertThat(map).hasSize(paths.size)
@@ -37,10 +43,10 @@ class PathUtilityKtTest {
 
     @Test
     fun releaseDebug() {
-        val paths = """
-            mylibrary\.externalNativeBuild\cmake\debug\arm64-v8a\build.ninja
-            mylibrary\.externalNativeBuild\cmake\release\armeabi-v7a\build.ninja
-        """.trimIndent().split("\n").map { Paths.get(it) }.toSet()
+        val paths = toPaths("""
+            mylibrary/.externalNativeBuild/cmake/debug/arm64-v8a/build.ninja
+            mylibrary/.externalNativeBuild/cmake/release/armeabi-v7a/build.ninja
+        """)
 
         val map = paths.removeCommonSegments()
         assertThat(map).hasSize(paths.size)
@@ -52,39 +58,39 @@ class PathUtilityKtTest {
 
     @Test
     fun bigTest() {
-        val paths = """
-            mylibrary\.externalNativeBuild\cmake\debug\arm64-v8a\build.ninja
-            mylibrary\.externalNativeBuild\cmake\debug\armeabi-v7a\build.ninja
-            mylibrary\.externalNativeBuild\cmake\debug\x86\build.ninja
-            mylibrary\.externalNativeBuild\cmake\debug\x86_64\build.ninja
-            mylibrary\.externalNativeBuild\cmake\release\armeabi-v7a\build.ninja
-            mylibrary\.externalNativeBuild\cmake\release\x86\build.ninja
-            mylibrary\.externalNativeBuild\cmake\release\x86_64\build.ninja
-            mylibrary-2\.externalNativeBuild\cmake\debug\arm64-v8a\build.ninja
-            mylibrary-2\.externalNativeBuild\cmake\debug\armeabi-v7a\build.ninja
-            mylibrary-2\.externalNativeBuild\cmake\debug\x86\build.ninja
-            mylibrary-2\.externalNativeBuild\cmake\debug\x86_64\build.ninja
-            mylibrary-2\.externalNativeBuild\cmake\release\arm64-v8a\build.ninja
-            mylibrary-2\.externalNativeBuild\cmake\release\armeabi-v7a\build.ninja
-            mylibrary-2\.externalNativeBuild\cmake\release\x86\build.ninja
-            mylibrary-2\.externalNativeBuild\cmake\release\x86_64\build.ninja
-            mylibrary-3\.externalNativeBuild\cmake\debug\arm64-v8a\build.ninja
-            mylibrary-3\.externalNativeBuild\cmake\debug\armeabi-v7a\build.ninja
-            mylibrary-3\.externalNativeBuild\cmake\debug\x86\build.ninja
-            mylibrary-3\.externalNativeBuild\cmake\debug\x86_64\build.ninja
-            mylibrary-3\.externalNativeBuild\cmake\release\arm64-v8a\build.ninja
-            mylibrary-3\.externalNativeBuild\cmake\release\armeabi-v7a\build.ninja
-            mylibrary-3\.externalNativeBuild\cmake\release\x86\build.ninja
-            mylibrary-3\.externalNativeBuild\cmake\release\x86_64\build.ninja
-            mylibrary-4\.externalNativeBuild\cmake\debug\arm64-v8a\build.ninja
-            mylibrary-4\.externalNativeBuild\cmake\debug\armeabi-v7a\build.ninja
-            mylibrary-4\.externalNativeBuild\cmake\debug\x86\build.ninja
-            mylibrary-4\.externalNativeBuild\cmake\debug\x86_64\build.ninja
-            mylibrary-4\.externalNativeBuild\cmake\release\arm64-v8a\build.ninja
-            mylibrary-4\.externalNativeBuild\cmake\release\armeabi-v7a\build.ninja
-            mylibrary-4\.externalNativeBuild\cmake\release\x86\build.ninja
-            mylibrary-4\.externalNativeBuild\cmake\release\x86_64\build.ninja
-        """.trimIndent().split("\n").map { Paths.get(it) }.toSet()
+        val paths = toPaths("""
+            mylibrary/.externalNativeBuild/cmake/debug/arm64-v8a/build.ninja
+            mylibrary/.externalNativeBuild/cmake/debug/armeabi-v7a/build.ninja
+            mylibrary/.externalNativeBuild/cmake/debug/x86/build.ninja
+            mylibrary/.externalNativeBuild/cmake/debug/x86_64/build.ninja
+            mylibrary/.externalNativeBuild/cmake/release/armeabi-v7a/build.ninja
+            mylibrary/.externalNativeBuild/cmake/release/x86/build.ninja
+            mylibrary/.externalNativeBuild/cmake/release/x86_64/build.ninja
+            mylibrary-2/.externalNativeBuild/cmake/debug/arm64-v8a/build.ninja
+            mylibrary-2/.externalNativeBuild/cmake/debug/armeabi-v7a/build.ninja
+            mylibrary-2/.externalNativeBuild/cmake/debug/x86/build.ninja
+            mylibrary-2/.externalNativeBuild/cmake/debug/x86_64/build.ninja
+            mylibrary-2/.externalNativeBuild/cmake/release/arm64-v8a/build.ninja
+            mylibrary-2/.externalNativeBuild/cmake/release/armeabi-v7a/build.ninja
+            mylibrary-2/.externalNativeBuild/cmake/release/x86/build.ninja
+            mylibrary-2/.externalNativeBuild/cmake/release/x86_64/build.ninja
+            mylibrary-3/.externalNativeBuild/cmake/debug/arm64-v8a/build.ninja
+            mylibrary-3/.externalNativeBuild/cmake/debug/armeabi-v7a/build.ninja
+            mylibrary-3/.externalNativeBuild/cmake/debug/x86/build.ninja
+            mylibrary-3/.externalNativeBuild/cmake/debug/x86_64/build.ninja
+            mylibrary-3/.externalNativeBuild/cmake/release/arm64-v8a/build.ninja
+            mylibrary-3/.externalNativeBuild/cmake/release/armeabi-v7a/build.ninja
+            mylibrary-3/.externalNativeBuild/cmake/release/x86/build.ninja
+            mylibrary-3/.externalNativeBuild/cmake/release/x86_64/build.ninja
+            mylibrary-4/.externalNativeBuild/cmake/debug/arm64-v8a/build.ninja
+            mylibrary-4/.externalNativeBuild/cmake/debug/armeabi-v7a/build.ninja
+            mylibrary-4/.externalNativeBuild/cmake/debug/x86/build.ninja
+            mylibrary-4/.externalNativeBuild/cmake/debug/x86_64/build.ninja
+            mylibrary-4/.externalNativeBuild/cmake/release/arm64-v8a/build.ninja
+            mylibrary-4/.externalNativeBuild/cmake/release/armeabi-v7a/build.ninja
+            mylibrary-4/.externalNativeBuild/cmake/release/x86/build.ninja
+            mylibrary-4/.externalNativeBuild/cmake/release/x86_64/build.ninja
+        """)
 
         val map = paths.removeCommonSegments()
         assertThat(map).hasSize(paths.size)
